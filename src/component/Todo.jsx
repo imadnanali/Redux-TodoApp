e0fae244306c4a65a941515d96eab8fa6a987012
Adnan <adnanali32721414114786@gmail.com>
2025-10-19 14:42:45 +0530

    files update

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -4,11 +4,11 @@ import AddTask from './AddTask'
 import { deleteTodo, markasDone } from '../features/todo/todoSlice'
 
 const Todo = () => {
-    const todos = useSelector((state)=>state.todos)
-    const dispatch = useDispatch()
-    console.log(todos)
+  const todos = useSelector((state) => state.todos)
+  const dispatch = useDispatch()
+  console.log(todos)
 
-  let clickHandler = (id)=>{
+  let clickHandler = (id) => {
     console.log(id)
     dispatch(deleteTodo(id))
   }
@@ -20,8 +20,8 @@ const Todo = () => {
       <AddTask />
       <h2>Todo List App</h2>
       <ul>
-        {todos.map((todo)=>(
-            <li key={todo.id}  style={{ color: todo.markasDone ? "red" : "black" }}>{todo.task} <button onClick={()=>clickHandler(todo.id)}>Delete</button> <button onClick={() => dispatch(markasDone(todo.id))}>Task Done</button></li>
+        {todos.map((todo) => (
+          <li key={todo.id} style={{ color: todo.isDone ? "red" : "black" }}>{todo.task} <button onClick={() => clickHandler(todo.id)}>Delete</button> <button onClick={() => dispatch(markasDone(todo.id))}>Task Done</button></li>
         ))}
       </ul>
     </div>
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,15 +1,15 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: nanoid(), task: "abc", isDone: false}],
+    todos: [{ id: nanoid(), task: "abc", isDone: false }],
 };
 
 
 export const todoSlice = createSlice({
     name: "todo",
     initialState,
-    reducers : { //state and task
-        addTodo: (state, action ) => {
+    reducers: { //state and task
+        addTodo: (state, action) => {
             const newTodo = {
                 id: nanoid(),
                 task: action.payload,
@@ -17,19 +17,18 @@ export const todoSlice = createSlice({
             }
             state.todos.push(newTodo);   //Direct mutation
         },
-        deleteTodo: (state, action)=>{
-            state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
-        markasDone : (state, action)=>{
-            state.todos = state.todos.map((todo)=>{
-                if(todo.id === action.payload){
-                    todo.isDone = true;
-                }
-            })
+        markasDone: (state, action) => {
+            const todo = state.todos.find((t) => t.id === action.payload);
+            if (todo) {
+                todo.isDone = true;
+            }
         }
     }
 })
 
 
-export const { addTodo, deleteTodo, markasDone } = todoSlice.actions;   
+export const { addTodo, deleteTodo, markasDone } = todoSlice.actions;
 export default todoSlice.reducer;
